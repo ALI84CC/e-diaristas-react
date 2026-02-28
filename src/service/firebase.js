97935ccc2c37356ac938e22dece0ab15b26dc495
alisonCosta <alison84cc@gmail.com>
2026-02-28 20:10:03 -0300

    "Alterações na segurança da aplicação para garantir melhor e mais seguro acesso"

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCk-EULAdDoEkUkUADoh2mu8TmgbcuQ9b4",
-  authDomain: "e-diarista.firebaseapp.com",
-  projectId: "e-diarista",
-  storageBucket: "e-diarista.firebasestorage.app",
-  messagingSenderId: "983636253828",
-  appId: "1:983636253828:web:a648cb3bae1e28557e76d1",
-  measurementId: "G-D7YYMHBRTM"
+ apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializa o Firebase
